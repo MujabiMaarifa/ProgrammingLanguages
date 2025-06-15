a3b097ba006ffde5ef2565fdcc75aeaa1cbb2109
Maarifa <daudimujabi@gmail.com>
2025-06-15 16:41:11 +0300

    new  commits

--- Calculator_DavidMujabi.py
+++ Calculator_DavidMujabi.py
@@ -1,9 +1,6 @@
 import math
-import os
 
-os.system("clear")
-
-global choice
+#function that contains the valid choices provided to the user
 def choices():
     print("Which operation do you want to perform?\n")
     print("1. Addition")
@@ -14,18 +11,20 @@ def choices():
     print("6. Power")
     print("7. Square Root")
     print("8. Convert to percentage")
-    print("9. Exit")
+    print("9. Load Calculation History")
+    print("10. Exit")
 
-    inputs = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+    inputs = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
     choice  = input("\nSelect one of the above choices: ")
-    if choice == "exit" or "q" or "quit" or "9":
-        print("Exiting the calculator..")
-        print("Thank you for using the calculator!! goodbye")
-        exit()
     if not choice.strip() or choice not in inputs:
         print("\nInvalid choice please enter a valid choice!!!\n")
         choices()
-    return choice
+    if choice == '10':
+        print("\nExiting the calculator!!!\n")
+        exit()
+    else:
+        return choice
+#function to handle user's input values
 def user_input():
     a = (input("Enter the first number: "))
     b = (input("Enter the second number: "))
@@ -36,49 +35,77 @@ def user_input():
         a = float(a)
         b = float(b)
         return a, b
+#function for the user to continue using the calculator or exit
 def user_continue():
-    print("Continue with calculation? (y/n)")
-    cont = input()
-    if cont.lower() == 'n' or cont.lower() == 'no' or cont.lower() == 'exit':
-        print("Exiting the calculator. Thank you for using..")
-        print("Maths is easy")
+    cont = input("Continue with calculation? (y/n): ")
+    if cont.lower() == 'n' or cont.lower() == 'no' or cont.lower() == 'exit' or cont.lower() == 'quit':
+        print("\nExiting the calculator. Thank you for using..")
+        print("\nProgramming and Maths is easy.. Just do it !.!")
         exit()
     else:
         return True
-    
+#function to save user's calculation history
+def save_calculations(operation, result):
+    try:
+        with open('calculations.txt', 'a') as file:
+            file.write(f"Operation: {operation}\nResult: {result}\n\n")
+            print("\nCalculations saved successfully.")
+    except FileNotFoundError:
+        print("\nError occurred while saving calculations.")
+#function to perform the operations based on user's input
 def calculator() :
     choice = choices()
-    a, b = user_input()
+    if choice in ['1', '2', '3', '4', '5', '6']:
+        a, b = user_input()  
     if choice == "1":
         result = a + b
         print("The result is: ", result)
+        save_calculations(f"{a} + {b} = ", result)
     elif choice == "2":
         result = a - b
         print("The result is: ", result)
+        save_calculations(f"{a} - {b} = ", result)
     elif choice == "3":
         result = a * b
         print("The product is: ", result)
+        save_calculations(f"{a} * {b} = ", result)
     elif choice == "4":
         if b == 0:
             print("Error!! Division by zero is not allowed.")
         else:
             result = a / b
             print("The quotient is: ", result)
+            save_calculations(f"{a} / {b} ", result)
     elif choice == "5":
         result = a % b
         print("The remainder is: ", result)
+        save_calculations(f"{a} % {b} ", result)
     elif choice == "6":
         result = pow(a, b)
         print(f"{a} to the power of {b} is: ", result)
+        save_calculations(f"{a} ^ {b} ", result)
     elif choice == "7":
         a = float(input("\nEnter the value whose square root is needed: "))
         result = math.sqrt(a)
         print(f"The square root of {a} is: ", result)
+        save_calculations(f"{a} square root: ", result)
     elif choice == "8":
         a = float(input("\nEnter the value to be convereted to percentage: "))
         result = a * 100
         print(f"{a} is {result} percent")
-    
+        save_calculations(f"{a} as percent is: ", result)
+    elif choice == "9":
+        print("The Calculations done are as shown below..\n")
+        with open('calculations.txt', 'r') as file:
+            print(file.read())
+            print("\nFile read successfully\n")
+    elif choice == "10":
+        print("Exiting the calculator..")
+        print("Thank you for using the calculator!! goodbye")
+        exit()
+    else:
+        print("Invalid choice. Please choose a valid option.")
+# main function that runs the calculator
 def main():
     print("Calculators make better mathematicians..")
     print("Welcome to your favourite calculator\n")
